68c11918c2e04d93d245ad3744d1d31da31b9245
marty <techno.mooney@gmail.com>
2022-05-07 15:36:39 -0500

    modified the visited states list to display a different message when there are no states visited.

--- states-client/src/components/StatesVisited.vue
+++ states-client/src/components/StatesVisited.vue
@@ -1,11 +1,14 @@
 <template>
-  <h2>These are the states you have visited:</h2>
+  <div v-if="visitedStates">
+    <h2>These are the states you have visited:</h2>
 
-<!--  found how to do this here: -->
-<!--  https://forum.vuejs.org/t/router-link-with-dynamic-id-in-v-for-loop/11040/5  -->
-  <p v-for="state in visitedStates" class="state-container">
-    <router-link :to="`/map/${state.name}`">{{ state.name }}</router-link>
-  </p>
+  <!--  found how to do this here: -->
+  <!--  https://forum.vuejs.org/t/router-link-with-dynamic-id-in-v-for-loop/11040/5  -->
+    <p v-for="state in visitedStates" class="state-container">
+      <router-link :to="`/map/${state.name}`">{{ state.name }}</router-link>
+    </p>
+  </div>
+  <h2 v-else>You have not visited any states yet!</h2>
 </template>
 
 <script>
