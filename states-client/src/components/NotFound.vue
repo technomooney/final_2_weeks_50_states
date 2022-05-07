8fd79f9e92aaf5b7af432977aece17bfcdaee1f3
marty <techno.mooney@gmail.com>
2022-05-07 15:29:37 -0500

    404 page not using a p tag, so it did not get the text color updates... fixed that

--- states-client/src/components/NotFound.vue
+++ states-client/src/components/NotFound.vue
@@ -4,7 +4,7 @@
 
     <div id="go-home-link">
       <!-- Navigation link with route name -->
-      Would you like to <router-link v-bind:to="{ name: 'StateList' }">go to the home page?</router-link>
+      <p>Would you like to <router-link v-bind:to="{ name: 'StateList' }">go to the home page?</router-link></p>
       <!-- Or we can use the path directly -->
       <!-- Would you like to <router-link to="/">go to the home page?</router-link> -->
     </div>
